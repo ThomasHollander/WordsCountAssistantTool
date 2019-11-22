f4b4582f3d4e7474b42f3f02ed87441c3c11e513
ThomasHollander <53078388+ThomasHollander@users.noreply.github.com>
2019-11-23 05:47:45 +0800

    Add files via upload

---
+++ WordCounterAssistantToolTests/WordCounterAssistantToolTests.swift
@@ -0,0 +1,17 @@
+import UIKit
+import XCTest
+class WordCounterTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    override func tearDown() {
+        super.tearDown()
+    }
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+    func testPerformanceExample() {
+        self.measure() {
+        }
+    }
+}
